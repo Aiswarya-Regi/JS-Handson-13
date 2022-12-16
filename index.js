8736641d5237bc15cf70d6ac6571399e303f1801
Aiswarya <aiswaryaregi69@gmail.com>
2022-12-16 22:59:59 +0530

    created JS Handson 13

---
+++ index.js
@@ -0,0 +1,11 @@
+let flexButton=document.getElementById("flex-function");
+let flexBox=document.getElementById("flex-box")
+flexButton.addEventListener("click",function(){
+    flexBox.style.flexDirection="column";
+})
+
+
+const selectValue=(e) =>{
+    document.getElementById("demo").innerHTML=document.getElementById("slct_id").value
+    
+}
